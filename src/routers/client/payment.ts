3c9d1ff0c3e8c98cd055dcb286733bb4fe1ab6fa
Marcelo Gonçalves <marcelo_goncalves@id.uff.br>
2021-09-03 11:14:07 -0300

    feat: payment validation schema added

--- src/routers/client/payment.ts
+++ src/routers/client/payment.ts
@@ -1,12 +1,11 @@
 import { Router } from "express";
 
 import * as controller from "@/controllers/client/payment";
-
-//import schemaValidatingMiddleware from "@/middlewares/schemaValidatingMiddleware";
-//import createNewUserSchema from "@/schemas/createNewUser";
+import schemaValidatingMiddleware from "@/middlewares/schemaValidatingMiddleware";
+import paymentSchema from "@/schemas/paymentSchema";
 
 const router = Router();
 
-router.post("/",  controller.createPayment);
+router.post("/", schemaValidatingMiddleware(paymentSchema),  controller.createPayment);
 
 export default router;

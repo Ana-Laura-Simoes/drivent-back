02bdfa6514828fdb8dc8e700ed31c438cc94dd01
Rodrigo Schmidt <rodrigodcsee@gmail.com>
2021-09-07 12:45:54 -0300

    test: hotels and rooms tests implemented

---
+++ tests/integration/hotels.test.ts
@@ -0,0 +1,31 @@
+import supertest from "supertest";
+import httpStatus from "http-status";
+
+import app, { init } from "../../src/app";
+import { clearDatabase, endConnection } from "../utils/database";
+import { createBasicSettings } from "../utils/app";
+
+const agent = supertest(app);
+
+beforeAll(async () => {
+  await init();
+});
+
+beforeEach(async () => {
+  await clearDatabase();
+  await createBasicSettings();
+});
+
+afterAll(async () => {
+  await clearDatabase();
+  await endConnection();
+});
+
+describe("GET /hotels", () => {
+  it("should respond with status 200", async () => {
+
+    const hotels = await agent.get("/hotels");
+
+    expect(hotels.statusCode).toEqual(httpStatus.OK);
+  });
+});

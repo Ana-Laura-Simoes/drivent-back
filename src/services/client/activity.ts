87688cc65d90bb84828638baf8836e9460aea2b0
Marcelo Gonçalves <marcelo_goncalves@id.uff.br>
2021-09-09 18:03:46 -0300

    feat: merged main

--- src/services/client/activity.ts
+++ src/services/client/activity.ts
@@ -1,7 +1,7 @@
 import Activity from "@/entities/Activity";
 
 export async function getDays() {
-  return await Activity.getActivities();
+  return await Activity.getDays();
 }
 
 export async function getActivitiesByDay(day: string) {

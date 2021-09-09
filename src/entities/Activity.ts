10ac3b013884104e5d776540758786709d6f60a7
Ana Simoes <ana.simoes-mato@unesp.br>
2021-09-09 00:05:11 -0300

    fix: get acivities by day

--- src/entities/Activity.ts
+++ src/entities/Activity.ts
@@ -1,5 +1,6 @@
 import { BaseEntity, Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm";
 import ActivityUser from "./ActivityUser";
+import { Like } from "typeorm";
 
 @Entity("activities")
 export default class Activity extends BaseEntity {
@@ -27,7 +28,27 @@ export default class Activity extends BaseEntity {
     @OneToMany(() => ActivityUser, (activityUser) => activityUser.activity)
     activityUser: ActivityUser;
 
+    static async getDayss() {
+      const v = await this.find({ select: ["beginTime"] });
+      console.log(v);
+      return v;
+    }
+
     static async getDays() {
-      return await this.find({ select: ["beginTime"] });
+      const beginTime="2021-10-21";
+
+      const response= await this.createQueryBuilder("activities").where("DATE(activities.beginTime) = :time", { time: beginTime }).getMany();
+      //this.createQueryBuilder("activities").where("DATE(activities.beginTime) = :time", { time: beginTime });
+      console.log( response);
+      return response;
+      //return await this.find({ where: { beginTime: "2021-10-22 09:00:00" } });
+      //.createQueryBuilder("activities").where("beginTime LIKE ‘%’ || :q || ‘%’", { q: beginTime }).getMany();
+      
+      //.find({ beginTime: Like(`%${beginTime}%`) }); 
+      
+      //.createQueryBuilder("activities")
+      //.where("activities.beginTime ilike :beginTime", { beginTime: `%${beginTime}%` })
+      //.getMany();
     }
 }
+
